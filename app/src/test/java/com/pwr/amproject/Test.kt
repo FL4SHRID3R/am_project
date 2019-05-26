f249123900c136695fe11d8c289cce774656c29b
Wojciech Najman <archlord.iinne@gmail.com>
2019-05-26 15:31:43 +0200

    work, work, work...

--- app/src/test/java/com/pwr/amproject/Test.kt
+++ app/src/test/java/com/pwr/amproject/Test.kt
@@ -11,8 +11,14 @@ class Test {
 
         coordinator = Coordinator(100, 50, 0, 4)
         coordinator.roundSwitcher()
-        
+
         coordinator = Coordinator(100, 50, 2, 2)
         coordinator.roundSwitcher()
+
+        val prize = 200
+        println(prize%1)
+        println(prize%2)
+        println((prize-2)%3)
+        println(prize%4)
     }
 }
